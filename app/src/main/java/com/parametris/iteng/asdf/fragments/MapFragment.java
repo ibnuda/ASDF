73ade111aacc7d1e8211978e9012ea0c0661344c
Ibnu <ibnu.d.a@student.uns.ac.id>
2016-08-02 20:42:16 +0700

    Something like google_maps_api or whatever.

--- app/src/main/java/com/parametris/iteng/asdf/fragments/MapFragment.java
+++ app/src/main/java/com/parametris/iteng/asdf/fragments/MapFragment.java
@@ -1,22 +1,30 @@
 package com.parametris.iteng.asdf.fragments;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.parametris.iteng.asdf.R;
 
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
-    GoogleMap googleMap;
+    GoogleMap myGoogleMap;
     MapView mapView;
     boolean mapSupported;
+    LocationManager locationManager;
+    Location here;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -34,7 +42,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     }
 
     private void initializeMap() {
-        if (googleMap == null && mapSupported) {
+        if (myGoogleMap == null && mapSupported) {
             mapView = (MapView) getActivity().findViewById(R.id.map_view);
             mapView.getMapAsync(this);
         }
@@ -44,11 +52,22 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
         mapView = (MapView) view.findViewById(R.id.map_view);
+        initializeMap();
         return view;
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        this.googleMap = googleMap;
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String bestProvider = locationManager.getBestProvider(criteria, true);
+        here = locationManager.getLastKnownLocation(bestProvider);
+
+        LatLng currentPosition = new LatLng(here.getLatitude(), here.getLongitude());
+
+        myGoogleMap = googleMap;
+        myGoogleMap.addMarker(new MarkerOptions().position(currentPosition).title("Position at the moment"));
+        myGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(currentPosition));
     }
+
 }
