24cbc0e1cb1aecfdefb9f3f5ef1d21e30c0c035a
DELL <iaji@parametris.com>
2016-08-08 16:48:07 +0700

    Track shown.

--- app/src/main/java/com/parametris/iteng/asdf/fragments/MyMapFragment.java
+++ app/src/main/java/com/parametris/iteng/asdf/fragments/MyMapFragment.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -20,8 +21,13 @@ import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.parametris.iteng.asdf.R;
 import com.parametris.iteng.asdf.track.LokService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MyMapFragment extends Fragment implements OnMapReadyCallback {
@@ -34,6 +40,8 @@ public class MyMapFragment extends Fragment implements OnMapReadyCallback {
     SharedPreferences sharedPreferences;
     Marker marker;
 
+    List<LatLng> routePoints = new ArrayList<LatLng>();
+    Polyline route;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -91,6 +99,12 @@ public class MyMapFragment extends Fragment implements OnMapReadyCallback {
                         cameraUpdate = CameraUpdateFactory.newLatLngZoom(here, 15);
                         googleMap.animateCamera(cameraUpdate);
                         googleMap.moveCamera(cameraUpdate);
+                        if (route != null) {
+                            route.remove();
+                        }
+                        routePoints.add(here);
+                        route = googleMap.addPolyline(new PolylineOptions().width(2).color(Color.BLACK).geodesic(true));
+                        route.setPoints(routePoints);
                         textViewHereIAm.setText(here.toString());
                     }
                 }, new IntentFilter(LokService.YUHU)
@@ -112,6 +126,7 @@ public class MyMapFragment extends Fragment implements OnMapReadyCallback {
         float lat = sharedPreferences.getFloat("prevLat", 0.0f);
         float lon = sharedPreferences.getFloat("prevLong", 0.0f);
         LatLng here = new LatLng(lat, lon);
+        routePoints.add(here);
         Log.d(TAG, "onMapReady: here : " + here.toString());
         cameraUpdate = CameraUpdateFactory.newLatLngZoom(here, 15);
 
@@ -121,6 +136,11 @@ public class MyMapFragment extends Fragment implements OnMapReadyCallback {
                 .title("Here I am.")
                 .draggable(true));
         marker.setVisible(true);
+
+        // while these ones shows the tracks.
+        route = googleMap.addPolyline(new PolylineOptions().width(2).color(Color.BLACK).geodesic(true));
+
+        route.setPoints(routePoints);
 
         googleMap.animateCamera(cameraUpdate);
         googleMap.moveCamera(cameraUpdate);
